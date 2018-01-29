2d38419333b30d10828b500c22ac1d46be7092bf
sfp123 <sfp123@missouristate.edu>
2018-01-29 13:13:52 -0600

    [initial import of csc-131 packing] more codeing

--- csc131/dishearneer.py
+++ csc131/dishearneer.py
@@ -1,3 +1,4 @@
+import string
 def get_personal_date() ->dict:
     """
     :return return a dir()
@@ -5,3 +6,25 @@ def get_personal_date() ->dict:
     personal_data = {"name": "Jimmy", "a_role": "dancer"}
     return personal_data
 
+
+def main() -> int:
+    default_dict = dict()
+    print(default_dict)
+    intitalized_dict = dict([('name','jimmy'),('a_role','joker')])
+    print(intitalized_dict)
+    simple_init_dict = dict(name='jimmy', a_role='dance star')
+    print(simple_init_dict)
+    simple_init_dict['a_role'] = 'was a bear'
+    print(simple_init_dict)
+    compre = {x: x**2 for x in range(5)}
+    print(compre)
+
+    print(string.punctuation)
+    s = "a little lamb, it's fleece".translate({ord(i): None for i in string.punctuation})
+    print(s)
+
+    return  0
+
+
+if __name__=='__main__':
+    main()
