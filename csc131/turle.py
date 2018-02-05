c39c999d66a7e41221d02469db9757cea36c4087
sfp123 <sfp123@missouristate.edu>
2018-02-05 13:04:43 -0600

    [initial import of csc-131 packing] more codeing

---
+++ csc131/turle.py
@@ -0,0 +1,22 @@
+import turtle
+
+def drawSquare(t, x, y, lenght):
+    t.up()
+    t.goto(x,y )
+    t.setheading(270)
+    t.down()
+    for count in range(4):
+        t.forward(lenght)
+        t.lest(90)
+
+def main():
+    wn = turtle.screen()
+    king = turtle.Turtle()
+    drawSquare(king,0, 0, 250)
+    #king.forward(150)
+   # king.left(90)
+    #king.forward(75)
+    turtle.exitonclick()
+
+if __name__=='__main__':
+    main()
