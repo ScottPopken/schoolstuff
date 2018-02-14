fd642a7808dc617aa77d0f2a69162c6166c57c73
ScottPopken <35503554+scottpopken@users.noreply.github.com>
2018-02-14 13:07:27 -0600

    [initial import of csc-131 packing] more codeing

---
+++ csc131/gie.py
@@ -0,0 +1,10 @@
+from breezypythongui import EasyFrame
+
+class LabelDemo(EasyFrame):
+    def __init__(self):
+        EasyFrame.__init__(self)
+        self.addLabel(text="hi", row=0, column=0)
+def main(self):
+     LabelDemo().mainloop()
+if __name__=="__main__":
+    main()
